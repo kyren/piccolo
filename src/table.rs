ead733b7eaf38f1aa7c6a13971e77768d3177262
kyren <kerriganw@gmail.com>
2018-07-25 13:56:20 -0400

    I *believe* this table growth logic matches vanilla Lua's closely enough
    The only caveat is that I think vanilla Lua will actually shrink both the array
    and map parts, but it only shrinks the array part when the map part exceeds its
    capacity, and shrinking the map part is done every time because it simply
    removes map nodes rather than having to resize a contiguous array like rust's
    HashMap.  This implementation simply never shrinks the array or map parts, but
    it would be reasonable to expose reserving / shrink to fit on the table somehow.

--- src/table.rs
+++ src/table.rs
@@ -10,7 +10,7 @@ use value::Value;
 
 #[derive(Debug, Copy, Clone, Collect)]
 #[collect(require_copy)]
-pub struct Table<'gc>(GcCell<'gc, TableParts<'gc>>);
+pub struct Table<'gc>(GcCell<'gc, TableState<'gc>>);
 
 #[derive(Fail, Debug)]
 pub enum InvalidTableKey {
@@ -36,41 +36,145 @@ impl<'gc> Hash for Table<'gc> {
 
 impl<'gc> Table<'gc> {
     pub fn new(&self, mc: MutationContext<'gc>) -> Table<'gc> {
-        Table(GcCell::allocate(
-            mc,
-            TableParts {
-                array: Vec::new(),
-                map: FnvHashMap::default(),
-            },
-        ))
+        Table(GcCell::allocate(mc, TableState::default()))
     }
 
     pub fn get(&self, key: Value<'gc>) -> Value<'gc> {
-        let parts = self.0.read();
-        match TableKey::new(key, parts.array.len()) {
-            Ok(TableKey::Array(i)) => parts.array[i],
-            Ok(TableKey::Map(k)) => parts.map.get(&k).cloned().unwrap_or(Value::Nil),
-            Err(_) => Value::Nil,
-        }
+        self.0.read().get(key)
     }
 
-    pub fn set(&self, mc: MutationContext<'gc>, key: Value<'gc>, value: Value<'gc>) -> Result<Value<'gc>, InvalidTableKey> {
-        let mut parts = self.0.write(mc);
-        match TableKey::new(key, parts.array.len())? {
-            TableKey::Array(i) => Ok(mem::replace(&mut parts.array[i], value)),
-            TableKey::Map(k) => if parts.map.len() < parts.map.capacity() {
-                Ok(parts.map.insert(k, value).unwrap_or(Value::Nil))
-            } else {
-                unimplemented!()
-            }
-        }
+    pub fn set(
+        &self,
+        mc: MutationContext<'gc>,
+        key: Value<'gc>,
+        value: Value<'gc>,
+    ) -> Result<Value<'gc>, InvalidTableKey> {
+        self.0.write(mc).set(key, value)
     }
 }
 
-#[derive(Debug, Collect)]
-struct TableParts<'gc> {
+#[derive(Debug, Collect, Default)]
+struct TableState<'gc> {
     array: Vec<Value<'gc>>,
     map: FnvHashMap<HashValue<'gc>, Value<'gc>>,
+}
+
+impl<'gc> TableState<'gc> {
+    fn get(&self, key: Value<'gc>) -> Value<'gc> {
+        if let Some(index) = to_array_index(key) {
+            if index < self.array.len() {
+                return self.array[index];
+            }
+        }
+
+        if let Ok(key) = HashValue::new(key) {
+            self.map.get(&key).cloned().unwrap_or(Value::Nil)
+        } else {
+            Value::Nil
+        }
+    }
+
+    fn set(&mut self, key: Value<'gc>, value: Value<'gc>) -> Result<Value<'gc>, InvalidTableKey> {
+        let index_key = to_array_index(key);
+        if let Some(index) = index_key {
+            if index < self.array.len() {
+                return Ok(mem::replace(&mut self.array[index], value));
+            }
+        }
+
+        let hash_key = HashValue::new(key)?;
+        if value == Value::Nil {
+            Ok(self.map.remove(&hash_key).unwrap_or(Value::Nil))
+        } else if self.map.len() < self.map.capacity() {
+            Ok(self.map.insert(hash_key, value).unwrap_or(Value::Nil))
+        } else {
+            // If a new element does not fit in either the array or map part of the table, we need
+            // to grow.  First, we find the total count of array candidate elements across the array
+            // part, the map part, and the newly inserted key.
+
+            const USIZE_BITS: usize = mem::size_of::<usize>() * 8;
+
+            // Count of array-candidate elements based on the highest bit in the index
+            let mut array_counts = [0; USIZE_BITS];
+            // Total count of all array-candidate elements
+            let mut array_total = 0;
+
+            for (i, e) in self.array.iter().enumerate() {
+                if *e != Value::Nil {
+                    array_counts[highest_bit(i)] += 1;
+                    array_total += 1;
+                }
+            }
+
+            for (k, _) in &self.map {
+                if let Some(i) = to_array_index(k.0) {
+                    array_counts[highest_bit(i)] += 1;
+                    array_total += 1;
+                }
+            }
+
+            if let Some(i) = index_key {
+                array_counts[highest_bit(i)] += 1;
+                array_total += 1;
+            }
+
+            // Then, we compute the new optimal size for the array by finding the largest array size
+            // such that at least half of the elements in the array would be in use.
+
+            let mut optimal_size = 0;
+            let mut total = 0;
+            for i in 0..USIZE_BITS {
+                if (1 << i) / 2 >= array_total {
+                    break;
+                }
+
+                if array_counts[i] > 0 {
+                    total += array_counts[i];
+                    if total > (1 << i) / 2 {
+                        optimal_size = 1 << i;
+                    }
+                }
+            }
+
+            let old_array_size = self.array.len();
+            let old_map_size = self.map.len();
+            if optimal_size > old_array_size {
+                // If we're growing the array part, we need to grow the array and take any newly valid
+                // array keys from the map part.
+
+                self.array.reserve(optimal_size - old_array_size);
+                let capacity = self.array.capacity();
+                self.array.resize(capacity, Value::Nil);
+
+                let array = &mut self.array;
+                self.map.retain(|k, v| {
+                    if let Some(i) = to_array_index(k.0) {
+                        if i < array.len() {
+                            array[i] = *v;
+                            return false;
+                        }
+                    }
+                    true
+                });
+            } else {
+                // If we aren't growing the array, we're adding a new element to the map that won't
+                // fit in the advertised capacity.  The capacity of std::collections::HashMap is
+                // just a lower-bound, so we may actually be able to insert past the capacity
+                // without the advertised capacity growing, so to make sure that we don't try to
+                // grow repeatedly, we need to make sure the capacity actually increases.  We simply
+                // double the capacity here.
+                self.map.reserve(old_map_size);
+            }
+
+            // Now we can insert the new key value pair
+            if let Some(index) = index_key {
+                if index < self.array.len() {
+                    return Ok(mem::replace(&mut self.array[index], value));
+                }
+            }
+            Ok(self.map.insert(hash_key, value).unwrap_or(Value::Nil))
+        }
+    }
 }
 
 // Value which implements Hash and Eq, and cannot contain Nil or NaN values.
@@ -130,32 +234,50 @@ fn canonical_float_bytes(f: f64) -> u64 {
     }
 }
 
-enum TableKey<'gc> {
-    Array(usize),
-    Map(HashValue<'gc>),
+// If the given key can live in the array part of the table (integral value between 1 and
+// usize::MAX), returns the associated array index.
+fn to_array_index<'gc>(key: Value<'gc>) -> Option<usize> {
+    let i = match key {
+        Value::Integer(i) => num_traits::cast::<_, usize>(i)?,
+        Value::Number(f) => {
+            let i = num_traits::cast::<_, usize>(f)?;
+            if i as f64 != f {
+                return None;
+            }
+            i
+        }
+        _ => {
+            return None;
+        }
+    };
+
+    if i > 0 {
+        Some(i - 1)
+    } else {
+        None
+    }
 }
 
-impl<'gc> TableKey<'gc> {
-    fn new(key: Value<'gc>, array_len: usize) -> Result<TableKey<'gc>, InvalidTableKey> {
-        let mut array_index = None;
-        match key {
-            Value::Integer(i) => array_index = num_traits::cast::<_, usize>(i),
-            Value::Number(f) => {
-                if let Some(i) = num_traits::cast::<_, usize>(f) {
-                    if i as f64 == f {
-                        array_index = Some(i);
-                    }
-                }
-            }
-            _ => {}
-        }
+// Returns the place of the highest set bit in the given i, i = 0 returns 0, i = 1 returns 1, i = 2
+// returns 2, i = 3 returns 2, and so on.
+fn highest_bit(mut i: usize) -> usize {
+    const LOG_2: [u8; 256] = [
+        0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+        5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+        6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+        7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+        7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+    ];
 
-        if let Some(ind) = array_index {
-            if ind < array_len {
-                return Ok(TableKey::Array(ind))
-            }
-        }
-
-        Ok(TableKey::Map(HashValue::new(key)?))
+    let mut hb = 0;
+    while i >= 256 {
+        hb += 8;
+        i = i >> 8;
     }
+
+    hb + LOG_2[i] as usize
 }
