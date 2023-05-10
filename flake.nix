c3515c5dc78d2d603e5041298719b04cfbab0232
kyren <kyren@kyju.org>
2023-05-10 10:09:36 -0400

    add flake.nix for easier development

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
+        };
+      in {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+            rust
+            cargo-edit
+          ];
+        };
+      }
+    );
+}
