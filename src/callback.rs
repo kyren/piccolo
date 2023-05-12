fac6f36b4d1bac72f9fdfdec4d255ea3d7e4f680
kyren <kyren@kyju.org>
2023-05-11 22:41:04 -0400

    repatriate unsafe code, the module separation is not that big of a deal

--- src/callback.rs
+++ src/callback.rs
@@ -1,7 +1,3 @@
-mod any_callback;
-
-pub use any_callback::AnyCallback;
-
 use std::{
     fmt,
     hash::{Hash, Hasher},
@@ -79,7 +75,71 @@ pub trait Callback<'gc>: Collect {
     ) -> Result<CallbackMode<'gc>, Error<'gc>>;
 }
 
+// Represents a callback as a single pointer with an inline VTable header.
+#[derive(Copy, Clone, Collect)]
+#[collect(no_drop, bound = "")]
+pub struct AnyCallback<'gc>(Gc<'gc, Header<'gc>>);
+
+struct Header<'gc> {
+    call: unsafe fn(
+        *const (),
+        MutationContext<'gc, '_>,
+        &mut Vec<Value<'gc>>,
+    ) -> Result<CallbackMode<'gc>, Error<'gc>>,
+}
+
 impl<'gc> AnyCallback<'gc> {
+    pub fn new<C: Callback<'gc> + 'gc>(mc: MutationContext<'gc, '_>, callback: C) -> Self {
+        #[repr(C)]
+        struct HeaderCallback<'gc, C> {
+            header: Header<'gc>,
+            callback: C,
+        }
+
+        // SAFETY: We can't auto-implement `Collect` due to the function pointer lifetimes, but
+        // function pointers can't hold any data. It would be nice if function pointers could have
+        // higher rank `for<'gc>` lifetimes.
+        unsafe impl<'gc, C: Collect> Collect for HeaderCallback<'gc, C> {
+            fn needs_trace() -> bool
+            where
+                Self: Sized,
+            {
+                C::needs_trace()
+            }
+
+            fn trace(&self, cc: gc_arena::CollectionContext) {
+                self.callback.trace(cc)
+            }
+        }
+
+        let hc = Gc::new(
+            mc,
+            HeaderCallback {
+                header: Header {
+                    call: |ptr, mc, stack| unsafe {
+                        let hc = ptr as *const HeaderCallback<C>;
+                        ((*hc).callback).call(mc, stack)
+                    },
+                },
+                callback,
+            },
+        );
+
+        Self(unsafe { Gc::cast::<Header>(hc) })
+    }
+
+    pub fn as_ptr(self) -> *const () {
+        Gc::as_ptr(self.0) as *const ()
+    }
+
+    pub fn call(
+        self,
+        mc: MutationContext<'gc, '_>,
+        stack: &mut Vec<Value<'gc>>,
+    ) -> Result<CallbackMode<'gc>, Error<'gc>> {
+        unsafe { (self.0.call)(Gc::as_ptr(self.0) as *const (), mc, stack) }
+    }
+
     pub fn from_fn<F>(mc: MutationContext<'gc, '_>, call: F) -> AnyCallback<'gc>
     where
         F: 'static
@@ -332,5 +392,40 @@ impl<'gc> AnyContinuation<'gc> {
         error: Error<'gc>,
     ) -> Result<CallbackMode<'gc>, Error<'gc>> {
         self.0.continue_err(mc, stack, error)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::CallbackReturn;
+
+    use gc_arena::rootless_arena;
+
+    use super::*;
+
+    #[test]
+    fn test_dyn_callback() {
+        rootless_arena(|mc| {
+            #[derive(Collect)]
+            #[collect(require_static)]
+            struct CB(i64);
+
+            impl<'gc> Callback<'gc> for CB {
+                fn call(
+                    &self,
+                    _: MutationContext<'gc, '_>,
+                    stack: &mut Vec<Value<'gc>>,
+                ) -> Result<CallbackMode<'gc>, Error<'gc>> {
+                    stack.push(Value::Integer(self.0));
+                    Ok(CallbackReturn::Return.into())
+                }
+            }
+
+            let dyn_callback = AnyCallback::new(mc, CB(17));
+
+            let mut stack = Vec::new();
+            assert!(dyn_callback.call(mc, &mut stack).is_ok());
+            assert!(matches!(stack[0], Value::Integer(17)));
+        })
     }
 }
