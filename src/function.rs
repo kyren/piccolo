b1368d753a056e30b13fa613deaa5d1b8803ca13
kyren <kyren@kyju.org>
2023-07-17 05:25:38 -0400

    cheaper Function::compose

--- src/function.rs
+++ src/function.rs
@@ -1,5 +1,4 @@
-use allocator_api2::vec;
-use gc_arena::{allocator_api::MetricsAlloc, Collect, Mutation};
+use gc_arena::{Collect, Gc, Mutation};
 
 use crate::{
     AnyCallback, AnySequence, CallbackReturn, Closure, Context, Error, Fuel, IntoMultiValue,
@@ -28,36 +27,41 @@ impl<'gc> From<AnyCallback<'gc>> for Function<'gc> {
 impl<'gc> Function<'gc> {
     pub fn compose<I>(mc: &Mutation<'gc>, functions: I) -> Self
     where
-        I: IntoIterator<Item = Function<'gc>> + Collect + Clone + 'gc,
+        I: AsRef<[Function<'gc>]> + Collect + 'gc,
     {
         #[derive(Collect)]
         #[collect(no_drop)]
-        struct Compose<'gc>(vec::Vec<Function<'gc>, MetricsAlloc<'gc>>);
+        struct Compose<'gc, I: 'gc>(Gc<'gc, I>, usize);
 
-        impl<'gc> Sequence<'gc> for Compose<'gc> {
+        impl<'gc, I> Sequence<'gc> for Compose<'gc, I>
+        where
+            I: AsRef<[Function<'gc>]> + Collect,
+        {
             fn poll(
                 &mut self,
                 _: Context<'gc>,
                 _: &mut Fuel,
                 _: &mut Stack<'gc>,
             ) -> Result<SequencePoll<'gc>, Error<'gc>> {
-                let function = self.0.pop().unwrap();
-                let is_tail = self.0.is_empty();
+                let fns = (*self.0).as_ref();
+                let function = fns[self.1];
+                self.1 += 1;
+                let is_tail = self.1 == fns.len();
                 Ok(SequencePoll::Call { function, is_tail })
             }
         }
 
         Self::Callback(AnyCallback::from_fn_with(
             mc,
-            functions,
+            Gc::new(mc, functions),
             |functions, ctx, _, _| {
-                let mut compose = Compose(vec::Vec::new_in(MetricsAlloc::new(&ctx)));
-                compose.0.extend(functions.clone());
-                if compose.0.is_empty() {
+                if (**functions).as_ref().is_empty() {
                     Ok(CallbackReturn::Return)
                 } else {
-                    compose.0.reverse();
-                    Ok(CallbackReturn::Sequence(AnySequence::new(&ctx, compose)))
+                    Ok(CallbackReturn::Sequence(AnySequence::new(
+                        &ctx,
+                        Compose(*functions, 0),
+                    )))
                 }
             },
         ))
@@ -70,9 +74,12 @@ impl<'gc> Function<'gc> {
         Self::Callback(AnyCallback::from_fn_with(
             mc,
             (self, args),
-            |(f, args), ctx, _, stack| {
+            |(f, args), ctx, fuel, stack| {
                 stack.into_front(ctx, args.clone());
-                Ok(CallbackReturn::TailCall(*f, None))
+                match *f {
+                    Function::Closure(c) => Ok(CallbackReturn::TailCall(c.into(), None)),
+                    Function::Callback(c) => c.call(ctx, fuel, stack),
+                }
             },
         ))
     }
