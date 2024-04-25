6f2aeca17d20aa09075c143e5927023a5f20f561
Jengamon <uche.okwo1@gmail.com>
2024-04-24 17:43:52 -0700

    fix: just use f64::log if base is given

--- src/stdlib/math.rs
+++ src/stdlib/math.rs
@@ -124,9 +124,6 @@ pub fn load_math<'gc>(ctx: Context<'gc>) {
         "log",
         callback("log", &ctx, |_, (v, base): (f64, Option<f64>)| match base {
             None => Some(v.ln()),
-            Some(f) if f == 2.0 => Some(v.log2()),
-            Some(f) if f == 10.0 => Some(v.log10()),
-            Some(f) if f == core::f64::consts::E => Some(v.ln()),
             Some(base) => Some(v.log(base)),
         }),
     )
