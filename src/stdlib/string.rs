9ac7fcdf687709a98eb8f41e3394b9b567a61ba2
Jengamon <uche.okwo1@gmail.com>
2024-04-27 18:26:33 -0700

    string.char implementation

--- src/stdlib/string.rs
+++ src/stdlib/string.rs
@@ -6,6 +6,57 @@ pub fn load_string<'gc>(ctx: Context<'gc>) {
     string
         .set(
             ctx,
+            "char",
+            Callback::from_fn(&ctx, |ctx, _, mut stack| {
+                let mut string = String::new();
+                for argn in 0..stack.len() {
+                    let codepoint = match stack.pop_front() {
+                        Value::Integer(i) if i >= 0 && i <= char::MAX as i64 => Ok(i as u32),
+                        Value::Number(n)
+                            if n >= 0.0 && n.fract() == 0.0 && n <= char::MAX as u32 as f64 =>
+                        {
+                            Ok(n as u32)
+                        }
+                        Value::String(s) => String::from_utf8(s.to_vec())
+                            .map_err(|_| format!("failed to decode argument #{argn} as UTF-8"))
+                            .and_then(|s| {
+                                s.parse::<f64>()
+                                    .map_err(|_| {
+                                        format!("failed to parse argument #{argn} as a number")
+                                    })
+                                    .and_then(|f| {
+                                        (f >= 0.0
+                                            && f.fract() == 0.0
+                                            && f <= char::MAX as u32 as f64)
+                                            .then_some(f as u32)
+                                            .ok_or(format!(
+                                                "argument #{argn} has no integer representation"
+                                            ))
+                                    })
+                            }),
+                        _ => Err(format!("argument #{argn} has no integer representation")),
+                    }
+                    .map_err(|s| s.into_value(ctx))?;
+
+                    if let Some(c) = char::from_u32(codepoint) {
+                        string.push(c);
+                    } else {
+                        return Err(format!(
+                            "argument #{argn}: {codepoint:x} is not a valid codepoint"
+                        )
+                        .into_value(ctx)
+                        .into());
+                    }
+                }
+                stack.replace(ctx, string);
+                Ok(CallbackReturn::Return)
+            }),
+        )
+        .unwrap();
+
+    string
+        .set(
+            ctx,
             "len",
             Callback::from_fn(&ctx, |ctx, _, mut stack| {
                 let v: Option<Value> = stack.consume(ctx)?;
