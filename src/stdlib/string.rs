cb84ec3eb65c4124d6abaf80e703f460576446d0
Jengamon <uche.okwo1@gmail.com>
2024-05-27 07:43:54 -0700

    Fix comments
    ...
    poking at stuff without a dev env is "*fun*"

--- src/stdlib/string.rs
+++ src/stdlib/string.rs
@@ -29,10 +29,39 @@ pub fn load_string<'gc>(ctx: Context<'gc>) {
             ctx,
             "sub",
             Callback::from_fn(&ctx, |ctx, _, mut stack| {
+                fn operate_sub<'a>(string: &'a [u8], i: i64, j: Option<i64>) -> Result<&'a [u8], std::num::TryFromIntError> {
+                    let i = if i > 0 {
+                        i.saturating_sub(1).try_into()?
+                    } else if i == 0 {
+                        0
+                    } else {
+                        string.len().saturating_add_signed(i.try_into()?)
+                    };
+                    let j = if let Some(j) = j {
+                        if j >= 0 {
+                            j.try_into()?
+                        } else {
+                            let j: isize = j.try_into()?;
+                            string.len().saturating_add_signed(j + 1)
+                        }
+                    } else {
+                        string.len()
+                    }
+                    .clamp(0, string.len());
+    
+                    return Ok(if i >= j || i >= string.len() {
+                        &[]
+                    } else {
+                        &string[i..j]
+                    })
+                }
+
                 let string = stack.pop_front();
+                let (i, j) = stack.consume::<(i64, Option<i64>)>(ctx)?;
                 let string = match string {
-                    Value::Integer(i) => i.to_string().as_bytes().to_vec(),
-                    Value::String(s) => s.as_bytes().to_vec(),
+                    Value::Integer(i) => ctx.intern(operate_sub(i.to_string().as_bytes(), i, j)?),
+                    Value::Number(n) => ctx.intern(operate_sub(n.to_string().as_bytes(), i, j)?),
+                    Value::String(s) => ctx.intern(operate_sub(s.as_bytes(), i, j)?),
                     v => {
                         return Err(format!(
                             "Bad argument to sub: expected string, got {}",
@@ -43,32 +72,7 @@ pub fn load_string<'gc>(ctx: Context<'gc>) {
                     }
                 };
 
-                let (i, j) = stack.consume::<(i64, Option<i64>)>(ctx)?;
-                let i = if i > 0 {
-                    i.saturating_sub(1).try_into()?
-                } else if i == 0 {
-                    0
-                } else {
-                    string.len().saturating_add_signed(i.try_into()?)
-                };
-                let j = if let Some(j) = j {
-                    if j >= 0 {
-                        j.try_into()?
-                    } else {
-                        let j: isize = j.try_into()?;
-                        string.len().saturating_add_signed(j + 1)
-                    }
-                } else {
-                    string.len()
-                }
-                .clamp(0, string.len());
-
-                let result = if i >= j || i >= string.len() {
-                    &[]
-                } else {
-                    &string[i..j]
-                };
-                stack.replace(ctx, crate::String::from_slice(&ctx, result).into_value(ctx));
+                stack.replace(ctx, string);
                 Ok(CallbackReturn::Return)
             }),
         )
