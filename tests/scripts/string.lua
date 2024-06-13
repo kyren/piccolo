77b549c68c43bb76a9ca8b5463b496c3e7530db6
Jengamon <bob.hostern@gmail.com>
2024-06-13 15:44:36 -0700

    fix: rebase

--- tests/scripts/string.lua
+++ tests/scripts/string.lua
@@ -3,10 +3,9 @@ function is_err(f)
 end
 
 do
-    assert(
-        "a" .. "b" .. "c" == "abc" and
-        "a" .. 1 .. "c" == "a1c" and
-        1 .. 2 .. 3 == "123")
+    assert("a" .. "b" .. "c" == "abc")
+    assert("a" .. 1 .. "c" == "a1c")
+    assert(1 .. 2 .. 3 == "123")
 end
 
 do
@@ -35,25 +34,28 @@ do
 end
 
 do
-    assert(
-        is_err(function() return string.len(nil) end) and
-        is_err(function() return string.len(true) end) and
-        is_err(function() return string.len(false) end) and
-        is_err(function() return string.len({}) end) and
-        is_err(function() return string.len(is_err) end) and
-        is_err(function() return string.len(coroutine.create(test_coroutine_len)) end) and
-        string.len("") == 0 and
-        string.len("x") == 1 and
-        string.len("x\0") == 2 and
-        string.len(1) == 1 and
-        string.len(-1) == 2 and
-        string.len(12) == 2 and
-        string.len(123) == 3 and
-        string.len(1.2) == 3 and
-        string.len(-1.2) == 4 and
-        string.len(1.23) == 4 and
-        string.len(2147483647) == 10 and
-        string.len(-2147483648) == 11)
+    assert(is_err(function() return string.sub(nil) end))
+    assert(is_err(function() return string.sub(true, 1) end))
+    assert(is_err(function() return string.sub(false) end))
+    assert(is_err(function() return string.sub({}) end))
+    assert(is_err(function() return string.sub(is_err) end))
+    assert(is_err(function() return string.sub(coroutine.create(test_coroutine_len)) end))
+    assert(string.sub(48, 1) == "48")
+    assert(string.sub(48, 2) == "8")
+    assert(string.sub(48, -1) == "8")
+    assert(string.sub("hilo", -1, -4) == "")
+    assert(string.sub("hilo", -4, 4) == "hilo")
+    assert(string.sub("hilo", -4, -3) == "hi")
+    assert(string.sub("hilo", -4, -6) == "")
+    assert(string.sub("hilo", 0, -4) == "h")
+    assert(string.sub(3.4, 1, 2) == "3.")
+end
+
+do
+    assert([[
+foo
+bar
+baz]] == "foo\nbar\nbaz")
 end
 
 do
