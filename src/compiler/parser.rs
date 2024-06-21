b4a602e54762176dfbf45f5e0b6833317022b782
Aeledfyr <aeledfyr@gmail.com>
2024-06-21 17:50:41 -0500

    Make idiv flooring, handle bitshift overflows, fix `~=` precedence

--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -1168,7 +1168,7 @@ fn binary_priority(operator: BinaryOperator) -> (u8, u8) {
         BinaryOperator::ShiftLeft => (7, 7),
         BinaryOperator::ShiftRight => (7, 7),
         BinaryOperator::Concat => (9, 8),
-        BinaryOperator::NotEqual => (10, 10),
+        BinaryOperator::NotEqual => (3, 3),
         BinaryOperator::Equal => (3, 3),
         BinaryOperator::LessThan => (3, 3),
         BinaryOperator::LessEqual => (3, 3),
