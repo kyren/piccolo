1ee5000b1499579fd356a140d775bfeedd658230
Aeledfyr <aeledfyr@gmail.com>
2024-06-30 17:31:57 -0500

    Refactor RawTable grow code into a method, use it in insert

--- src/table/raw.rs
+++ src/table/raw.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, fmt, hash::Hash, i64, mem};
+use std::{fmt, hash::Hash, i64, mem};
 
 use allocator_api2::vec;
 use gc_arena::{allocator_api::MetricsAlloc, Collect, Gc, Mutation};
@@ -142,100 +142,7 @@ impl<'gc> RawTable<'gc> {
         } else if self.map.len() < self.map.capacity() {
             set_reserved_value(&mut self.map, hash, table_key, value)
         } else {
-            // If a new element does not fit in either the array or map part of the table, we need
-            // to grow. First, we find the total count of array candidate elements across the array
-            // part, the map part, and the newly inserted key.
-
-            const USIZE_BITS: usize = mem::size_of::<usize>() * 8;
-
-            // Count of array-candidate elements based on the highest bit in the index
-            let mut array_counts = [0; USIZE_BITS];
-            // Total count of all array-candidate elements
-            let mut array_total = 0;
-
-            for (i, e) in self.array.iter().enumerate() {
-                if !e.is_nil() {
-                    array_counts[highest_bit(i)] += 1;
-                    array_total += 1;
-                }
-            }
-
-            for (&key, &value) in &self.map {
-                if !value.is_nil() {
-                    if let Some(i) = to_array_index(
-                        key.live_key()
-                            .expect("dead keys must have Nil values")
-                            .to_value(),
-                    ) {
-                        array_counts[highest_bit(i)] += 1;
-                        array_total += 1;
-                    }
-                }
-            }
-
-            if let Some(i) = index_key {
-                array_counts[highest_bit(i)] += 1;
-                array_total += 1;
-            }
-
-            // Then, we compute the new optimal size for the array by finding the largest array size
-            // such that at least half of the elements in the array would be in use.
-
-            let mut optimal_size = 0;
-            let mut total = 0;
-            for i in 0..USIZE_BITS {
-                if (1 << i) / 2 >= array_total {
-                    break;
-                }
-
-                if array_counts[i] > 0 {
-                    total += array_counts[i];
-                    if total > (1 << i) / 2 {
-                        optimal_size = 1 << i;
-                    }
-                }
-            }
-
-            let old_array_size = self.array.len();
-            let old_map_size = self.map.len();
-            if optimal_size > old_array_size {
-                // If we're growing the array part, we need to grow the array and take any newly
-                // valid array keys from the map part.
-
-                self.array.reserve(optimal_size - old_array_size);
-                let capacity = self.array.capacity();
-                self.array.resize(capacity, Value::Nil);
-
-                let array = &mut self.array;
-                self.map.retain(|k, v| {
-                    if v.is_nil() {
-                        // If our entry is dead, remove it.
-                        return false;
-                    }
-
-                    let key = k.live_key().expect("all dead keys should have a Nil value");
-
-                    // If our live key is an array index that fits in the array portion,
-                    // move the entry to the array portion.
-                    if let Some(i) = to_array_index(key.to_value()) {
-                        if i < array.len() {
-                            array[i] = *v;
-                            return false;
-                        }
-                    }
-
-                    true
-                });
-            } else {
-                // If we aren't growing the array, we're adding a new element to the map that won't
-                // fit in the advertised capacity. We explicitly double the map size here.
-                self.map.raw_table_mut().reserve(old_map_size, |(key, _)| {
-                    self.hash_builder.hash_one(
-                        key.live_key()
-                            .expect("all keys must be live when table is grown"),
-                    )
-                });
-            }
+            self.grow(index_key);
 
             // Now we can insert the new key value pair
             match index_key {
@@ -245,6 +152,103 @@ impl<'gc> RawTable<'gc> {
                 _ => set_reserved_value(&mut self.map, hash, table_key, value),
             }
         })
+    }
+
+    fn grow(&mut self, index_key: Option<usize>) {
+        // If a new element does not fit in either the array or map part of the table, we need
+        // to grow. First, we find the total count of array candidate elements across the array
+        // part, the map part, and the newly inserted key.
+
+        const USIZE_BITS: usize = mem::size_of::<usize>() * 8;
+
+        // Count of array-candidate elements based on the highest bit in the index
+        let mut array_counts = [0; USIZE_BITS];
+        // Total count of all array-candidate elements
+        let mut array_total = 0;
+
+        for (i, e) in self.array.iter().enumerate() {
+            if !e.is_nil() {
+                array_counts[highest_bit(i)] += 1;
+                array_total += 1;
+            }
+        }
+
+        for (&key, &value) in &self.map {
+            if !value.is_nil() {
+                if let Some(i) = to_array_index(
+                    key.live_key()
+                        .expect("dead keys must have Nil values")
+                        .to_value(),
+                ) {
+                    array_counts[highest_bit(i)] += 1;
+                    array_total += 1;
+                }
+            }
+        }
+
+        if let Some(i) = index_key {
+            array_counts[highest_bit(i)] += 1;
+            array_total += 1;
+        }
+
+        // Then, we compute the new optimal size for the array by finding the largest array size
+        // such that at least half of the elements in the array would be in use.
+
+        let mut optimal_size = 0;
+        let mut total = 0;
+        for i in 0..USIZE_BITS {
+            if (1 << i) / 2 >= array_total {
+                break;
+            }
+
+            if array_counts[i] > 0 {
+                total += array_counts[i];
+                if total > (1 << i) / 2 {
+                    optimal_size = 1 << i;
+                }
+            }
+        }
+
+        let old_array_size = self.array.len();
+        let old_map_size = self.map.len();
+        if optimal_size > old_array_size {
+            // If we're growing the array part, we need to grow the array and take any newly
+            // valid array keys from the map part.
+
+            self.array.reserve(optimal_size - old_array_size);
+            let capacity = self.array.capacity();
+            self.array.resize(capacity, Value::Nil);
+
+            let array = &mut self.array;
+            self.map.retain(|k, v| {
+                if v.is_nil() {
+                    // If our entry is dead, remove it.
+                    return false;
+                }
+
+                let key = k.live_key().expect("all dead keys should have a Nil value");
+
+                // If our live key is an array index that fits in the array portion,
+                // move the entry to the array portion.
+                if let Some(i) = to_array_index(key.to_value()) {
+                    if i < array.len() {
+                        array[i] = *v;
+                        return false;
+                    }
+                }
+
+                true
+            });
+        } else {
+            // If we aren't growing the array, we're adding a new element to the map that won't
+            // fit in the advertised capacity. We explicitly double the map size here.
+            self.map.raw_table_mut().reserve(old_map_size, |(key, _)| {
+                self.hash_builder.hash_one(
+                    key.live_key()
+                        .expect("all keys must be live when table is grown"),
+                )
+            });
+        }
     }
 
     pub fn length(&self) -> i64 {
@@ -386,9 +390,16 @@ impl<'gc> RawTable<'gc> {
         NextValue::NotFound
     }
 
-    // Returns the removed key, if any, and the result of length()
-    //
-    // `key` is 1-indexed.
+    /// Try to efficiently remove a key from the array part of the
+    /// table.  (`key` is one-indexed; if it is None, the length of
+    /// the array is used instead.)
+    ///
+    /// If successful, returns the removed value; otherwise, indicates
+    /// whether the operation is possible to implement with a fallback,
+    /// or is impossible due to an out-of-range index.
+    ///
+    /// Additionally, always returns the computed length of the array
+    /// from before the operation.
     pub fn array_remove_shift(
         &mut self,
         key: Option<i64>,
@@ -429,9 +440,17 @@ impl<'gc> RawTable<'gc> {
         RawArrayOpResult::Success(value)
     }
 
-    // Returns whether the insert succeeded, and the result of length()
-    //
-    // `key` is 1-indexed.
+    /// Try to efficiently insert a key and value into the array part
+    /// of the table.  (`key` is one-indexed; if it is `None`, the
+    /// length of the array is used instead.)
+    ///
+    /// The returned [`RawArrayOpResult`] indicates whether the
+    /// operation was successful, or if it failed,
+    /// whether the operation is possible to implement with a fallback,
+    /// or is impossible due to an out-of-range index.
+    ///
+    /// Additionally, always returns the computed length of the array
+    /// from before the operation.
     pub fn array_insert_shift(
         &mut self,
         key: Option<i64>,
@@ -461,33 +480,24 @@ impl<'gc> RawTable<'gc> {
             return RawArrayOpResult::Possible;
         }
 
-        match index.cmp(&length) {
-            Ordering::Greater => unreachable!(),
-            Ordering::Equal => {
-                // Length guarantees that (0-inx) arr[length] == nil,
-                // so it shouldn't be in the map. (TODO: check; dead keys?)
-                if index == self.array.len() {
-                    // TODO: move array growing logic out of set
-                    self.array.push(value);
-                } else {
-                    self.array[length] = value;
-                }
-                RawArrayOpResult::Success(())
-            }
-            Ordering::Less => {
-                // Length guarantees that (0-inx) arr[length] == nil,
-                // so it shouldn't be in the map. (TODO: check; dead keys?)
-                if length == self.array.len() {
-                    // TODO: move array growing logic out of set
-                    self.array.push(value); // Make rotate do the work for us
-                    self.array[index..=length].rotate_right(1);
-                } else {
-                    self.array[index] = value;
-                    self.array[index..=length].rotate_right(1);
-                }
-                RawArrayOpResult::Success(())
+        assert!(index <= length);
+
+        if length == self.array.len() {
+            // If the array is full, try to grow it.
+            self.grow(Some(index));
+            if length >= self.array.len() {
+                // If resize didn't grow the array, use fallback impl
+                return RawArrayOpResult::Possible;
             }
         }
+
+        // We know here that length < array.len(), so we shift each
+        // element to the right by one.
+        // array[length] == nil, which gets rotated back to array[index];
+        // we replace it with the value to insert.
+        self.array[index..=length].rotate_right(1);
+        self.array[index] = value;
+        RawArrayOpResult::Success(())
     }
 
     pub fn reserve_array(&mut self, additional: usize) {
