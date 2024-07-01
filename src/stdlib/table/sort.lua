efb5ddbab7d99480c10e22362166eb7e936434a7
Aeledfyr <aeledfyr@gmail.com>
2024-07-01 16:06:21 -0500

    Add support for comparison function in table.sort

--- src/stdlib/table/sort.lua
+++ src/stdlib/table/sort.lua
@@ -16,24 +16,46 @@ local function merge(src, dst, lo, mid, hi)
     end
 end
 
+-- merge, but uses the given comparison function
+local function merge_cmp(src, dst, lo, mid, hi, cmp)
+    local i = lo
+    local j = mid
+    for k = lo, hi do
+        if i < mid and (j > hi or cmp(src[i], src[j])) then
+            dst[k] = src[i]
+            i = i + 1
+        else
+            dst[k] = src[j]
+            j = j + 1
+        end
+    end
+end
+
 -- sorts arr[lo ..= hi] (inclusive, one-indexed)
-local function sort(arr, lo, hi, t)
+local function sort(arr, lo, hi, t, cmp)
     local n = hi - (lo - 1)
     if n <= 1 then
         return
     end
     local mid = lo + n // 2
-    sort(arr, lo, mid - 1, t)
-    sort(arr, mid, hi, t)
+    sort(arr, lo, mid - 1, t, cmp)
+    sort(arr, mid, hi, t, cmp)
     for i = lo, hi do
         t[i] = arr[i]
     end
-    merge(t, arr, lo, mid, hi)
+    if cmp == nil then
+        merge(t, arr, lo, mid, hi)
+    else
+        merge_cmp(t, arr, lo, mid, hi, cmp)
+    end
 end
 
-local arr = ...
+local arr, cmp = ...
 local len = #arr
+if len < 0 then
+    len = 0
+end
 
 local temp = {}
-sort(arr, 1, len, temp)
+sort(arr, 1, len, temp, cmp)
 return
