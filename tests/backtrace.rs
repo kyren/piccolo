b603a5c94ac4e07fd250d458218ad3ebd1243515
Robert Bragg <robert@sixbynine.org>
2025-07-10 22:34:55 +0100

    tests/backtrace: unwrap error + backtrace with let else and dedent code

--- tests/backtrace.rs
+++ tests/backtrace.rs
@@ -20,126 +20,126 @@ fn test_backtrace() {
     let mut source_map = std::collections::HashMap::new();
     source_map.insert("test_backtrace.lua".to_string(), SCRIPT.to_string());
 
-    if let Err(
+    let Err(
         ref err @ ExternError::Lua {
             ref error,
             ref backtrace,
             ..
         },
     ) = result
+    else {
+        panic!("expected an error, got: {:?}", result);
+    };
+
+    assert!(error.to_string().contains("test error from rust"));
+
+    let backtrace = backtrace.as_ref().unwrap();
+    let mut backtrace_string = String::new();
+    eprintln!("Error: {err:#}");
+    err.pretty_print(&mut backtrace_string, Some(&source_map))
+        .unwrap();
+    eprintln!("Backtrace: {backtrace_string}");
+    assert_eq!(backtrace.len(), 5);
+
+    if let BacktraceFrame::Lua {
+        chunk_name,
+        function_name,
+        line_number,
+        args,
+    } = &backtrace[0]
     {
-        assert!(error.to_string().contains("test error from rust"));
-
-        let backtrace = backtrace.as_ref().unwrap();
-        let mut backtrace_string = String::new();
-        eprintln!("Error: {err:#}");
-        err.pretty_print(&mut backtrace_string, Some(&source_map))
-            .unwrap();
-        eprintln!("Backtrace: {backtrace_string}");
-        assert_eq!(backtrace.len(), 5);
-
-        if let BacktraceFrame::Lua {
-            chunk_name,
-            function_name,
-            line_number,
+        assert_eq!(chunk_name, "test_backtrace.lua");
+        assert!(function_name.starts_with("<function 'error_callback'"));
+        assert_eq!(line_number.0, 3);
+        assert_eq!(
             args,
-        } = &backtrace[0]
-        {
-            assert_eq!(chunk_name, "test_backtrace.lua");
-            assert!(function_name.starts_with("<function 'error_callback'"));
-            assert_eq!(line_number.0, 3);
-            assert_eq!(
-                args,
-                &vec![("err_msg".to_string(), "test error from rust".to_string())]
-            );
-        } else {
-            panic!("Expected first frame to be Lua frame");
-        }
-
-        if let BacktraceFrame::Lua {
-            chunk_name,
-            function_name,
-            line_number,
-            args,
-        } = &backtrace[1]
-        {
-            assert_eq!(chunk_name, "test_backtrace.lua");
-            assert!(function_name.starts_with("<function 'baz' at line 7>"));
-            assert_eq!(line_number.0, 8);
-            assert_eq!(
-                args,
-                &vec![
-                    ("a".to_string(), "test".to_string()),
-                    ("b".to_string(), " error from rust".to_string())
-                ]
-            );
-        } else {
-            panic!("Expected second frame to be Lua frame");
-        }
-
-        if let BacktraceFrame::Lua {
-            chunk_name,
-            function_name,
-            line_number,
-            args,
-        } = &backtrace[2]
-        {
-            assert_eq!(chunk_name, "test_backtrace.lua");
-            assert!(function_name.starts_with("<function 'bar' at line 12>"));
-            assert_eq!(line_number.0, 12);
-            assert_eq!(
-                args,
-                &vec![
-                    ("x".to_string(), "test".to_string()),
-                    ("...".to_string(), " error from rust".to_string())
-                ]
-            );
-        } else {
-            panic!("Expected third frame to be Lua frame");
-        }
-
-        if let BacktraceFrame::Lua {
-            chunk_name,
-            function_name,
-            line_number,
-            args,
-        } = &backtrace[3]
-        {
-            assert_eq!(chunk_name, "test_backtrace.lua");
-            assert!(function_name.starts_with("<function 'foo' at line 16>"));
-            assert_eq!(line_number.0, 18);
-            assert_eq!(args, &vec![("arg1".to_string(), "test".to_string())]);
-        } else {
-            panic!("Expected fourth frame to be Lua frame");
-        }
-
-        if let BacktraceFrame::Lua {
-            chunk_name,
-            function_name,
-            line_number,
-            args,
-        } = &backtrace[4]
-        {
-            assert_eq!(chunk_name, "test_backtrace.lua");
-            assert_eq!(function_name, "<chunk>");
-            assert_eq!(line_number.0, 21);
-            assert!(args.is_empty());
-        } else {
-            panic!("Expected fifth frame to be Chunk frame");
-        }
-
-        let mut output = String::new();
-        err.pretty_print(&mut output, Some(&source_map)).unwrap();
-
-        // Check we lookup the correct source map entries without being too pedantic about the format
-        assert!(output.contains("-- This is line 4"));
-        assert!(output.contains("-- This is line 9"));
-        assert!(output.contains("-- This is line 13"));
-        assert!(output.contains("-- This is line 19"));
-        assert!(output.contains("-- This is line 22"));
+            &vec![("err_msg".to_string(), "test error from rust".to_string())]
+        );
     } else {
-        panic!("unexpected result type: {:?}", result);
+        panic!("Expected first frame to be Lua frame");
     }
+
+    if let BacktraceFrame::Lua {
+        chunk_name,
+        function_name,
+        line_number,
+        args,
+    } = &backtrace[1]
+    {
+        assert_eq!(chunk_name, "test_backtrace.lua");
+        assert!(function_name.starts_with("<function 'baz' at line 7>"));
+        assert_eq!(line_number.0, 8);
+        assert_eq!(
+            args,
+            &vec![
+                ("a".to_string(), "test".to_string()),
+                ("b".to_string(), " error from rust".to_string())
+            ]
+        );
+    } else {
+        panic!("Expected second frame to be Lua frame");
+    }
+
+    if let BacktraceFrame::Lua {
+        chunk_name,
+        function_name,
+        line_number,
+        args,
+    } = &backtrace[2]
+    {
+        assert_eq!(chunk_name, "test_backtrace.lua");
+        assert!(function_name.starts_with("<function 'bar' at line 12>"));
+        assert_eq!(line_number.0, 12);
+        assert_eq!(
+            args,
+            &vec![
+                ("x".to_string(), "test".to_string()),
+                ("...".to_string(), " error from rust".to_string())
+            ]
+        );
+    } else {
+        panic!("Expected third frame to be Lua frame");
+    }
+
+    if let BacktraceFrame::Lua {
+        chunk_name,
+        function_name,
+        line_number,
+        args,
+    } = &backtrace[3]
+    {
+        assert_eq!(chunk_name, "test_backtrace.lua");
+        assert!(function_name.starts_with("<function 'foo' at line 16>"));
+        assert_eq!(line_number.0, 18);
+        assert_eq!(args, &vec![("arg1".to_string(), "test".to_string())]);
+    } else {
+        panic!("Expected fourth frame to be Lua frame");
+    }
+
+    if let BacktraceFrame::Lua {
+        chunk_name,
+        function_name,
+        line_number,
+        args,
+    } = &backtrace[4]
+    {
+        assert_eq!(chunk_name, "test_backtrace.lua");
+        assert_eq!(function_name, "<chunk>");
+        assert_eq!(line_number.0, 21);
+        assert!(args.is_empty());
+    } else {
+        panic!("Expected fifth frame to be Chunk frame");
+    }
+
+    let mut output = String::new();
+    err.pretty_print(&mut output, Some(&source_map)).unwrap();
+
+    // Check we lookup the correct source map entries without being too pedantic about the format
+    assert!(output.contains("-- This is line 4"));
+    assert!(output.contains("-- This is line 9"));
+    assert!(output.contains("-- This is line 13"));
+    assert!(output.contains("-- This is line 19"));
+    assert!(output.contains("-- This is line 22"));
 }
 
 #[test]
